705b191ee3958981424a88e11f003292f337fa2e
Alin Babu <alinbabu2010@outlook.com>
2023-08-21 12:49:28 +0530

    Add contact list state

---
+++ shared/src/commonMain/kotlin/com/composemultiplatform/contactsapp/contacts/presentation/ContactsListState.kt
@@ -0,0 +1,14 @@
+package com.composemultiplatform.contactsapp.contacts.presentation
+
+import com.composemultiplatform.contactsapp.contacts.domain.Contact
+
+data class ContactsListState(
+    val contacts: List<Contact> = emptyList(),
+    val recentlyAddedContacts: List<Contact> = emptyList(),
+    val selectedContact: Contact? = null,
+    val isAddContactSheetOpen: Boolean = false,
+    val firstNameError: String? = null,
+    val lastNameError: String? = null,
+    val emailError: String? = null,
+    val phoneNumberError: String? = null
+)
