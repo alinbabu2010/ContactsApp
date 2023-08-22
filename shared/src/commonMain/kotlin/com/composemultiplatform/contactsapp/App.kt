b7da4e24d5e6a557db441df6ed41a40192aaa958
Alin Babu <alinbabu2010@outlook.com>
2023-08-22 15:10:31 +0530

    Add composable for launching app UI with theme

---
+++ shared/src/commonMain/kotlin/com/composemultiplatform/contactsapp/App.kt
@@ -0,0 +1,47 @@
+package com.composemultiplatform.contactsapp
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.composemultiplatform.contactsapp.contacts.presentation.ContactListScreen
+import com.composemultiplatform.contactsapp.contacts.presentation.ContactsListViewModel
+import com.composemultiplatform.contactsapp.core.presentation.ContactsTheme
+import dev.icerock.moko.mvvm.compose.getViewModel
+import dev.icerock.moko.mvvm.compose.viewModelFactory
+
+@Composable
+fun App(
+    darkTheme: Boolean,
+    dynamicColor: Boolean
+) {
+
+    ContactsTheme(
+        darkTheme = darkTheme,
+        dynamicColor = dynamicColor
+    ) {
+        val viewModel = getViewModel(
+            key = Unit,
+            factory = viewModelFactory {
+                ContactsListViewModel()
+            }
+        )
+        val uiState by viewModel.uiState.collectAsState()
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+
+            ContactListScreen(
+                state = uiState,
+                newContact = viewModel.newContact,
+                onEvent = viewModel::onEvent
+            )
+
+        }
+    }
+
+}
