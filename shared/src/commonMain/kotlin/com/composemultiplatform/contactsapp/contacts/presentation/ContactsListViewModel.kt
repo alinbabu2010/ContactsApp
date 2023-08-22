8dbc14147ee4a8e992a659d857c34dde68794964
Alin Babu <alinbabu2010@outlook.com>
2023-08-22 19:30:42 +0530

    Add viewModel event functions and refactor uiState to use data source
    Cut dummy contact data and refactor code

--- shared/src/commonMain/kotlin/com/composemultiplatform/contactsapp/contacts/presentation/ContactsListViewModel.kt
+++ shared/src/commonMain/kotlin/com/composemultiplatform/contactsapp/contacts/presentation/ContactsListViewModel.kt
@@ -4,36 +4,197 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import com.composemultiplatform.contactsapp.contacts.domain.Contact
+import com.composemultiplatform.contactsapp.contacts.domain.ContactDataSource
+import com.composemultiplatform.contactsapp.contacts.domain.ContactValidator
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class ContactsListViewModel : ViewModel() {
+class ContactsListViewModel(
+    private val contactDataSource: ContactDataSource
+) : ViewModel() {
 
-    private val contacts = (1..50).map {
-        Contact(
-            id = it.toLong(),
-            firstName = "First$it",
-            lastName = "Last$it",
-            email = "first$it@mail.com",
-            phoneNumber = "15362548$it",
-            photoBytes = null
+    private val _uiState = MutableStateFlow(ContactsListState())
+    val uiState = combine(
+        _uiState,
+        contactDataSource.getContacts(),
+        contactDataSource.getRecentContacts(10)
+    ) { state, contacts, recentContacts ->
+        state.copy(
+            contacts = contacts,
+            recentlyAddedContacts = recentContacts
         )
-    }
-
-    private val _uiState = MutableStateFlow(
-        ContactsListState(
-            contacts = contacts
-        )
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5000L),
+        ContactsListState()
     )
-    val uiState = _uiState.asStateFlow()
 
     var newContact: Contact? by mutableStateOf(null)
         private set
 
-    fun onEvent(contactListEvent: ContactListEvent) {
-
+    fun onEvent(event: ContactListEvent) {
+        when (event) {
+            ContactListEvent.DeleteContact -> deleteContact()
+            ContactListEvent.DismissContact -> dismissContact()
+            ContactListEvent.OnAddNewContactClick -> onAddNewContactClick()
+            ContactListEvent.SaveContact -> saveContact()
+            ContactListEvent.OnAddPhotoClicked -> {}
+            is ContactListEvent.EditContact -> editContact(event.contact)
+            is ContactListEvent.OnEmailChanged -> onEmailChanged(event.value)
+            is ContactListEvent.OnFirstNameChanged -> onFirstNameChanged(event.value)
+            is ContactListEvent.OnLastNameChanged -> onLastNameChanged(event.value)
+            is ContactListEvent.OnPhoneNumberChanged -> onPhoneNumberChanged(event.value)
+            is ContactListEvent.OnPhotoPicked -> onPhotoPicked(event.byteArray)
+            is ContactListEvent.SelectContact -> selectContact(event.contact)
+        }
     }
 
+    private fun saveContact() {
+        newContact?.let { contact ->
+            val result = ContactValidator.validateContact(contact)
+            val errors = listOfNotNull(
+                result.firstNameError,
+                result.lastNameError,
+                result.emailError,
+                result.phoneNumberError
+            )
+
+            if (errors.isEmpty()) {
+                _uiState.update {
+                    it.copy(
+                        isAddContactSheetOpen = false,
+                        firstNameError = null,
+                        lastNameError = null,
+                        emailError = null,
+                        phoneNumberError = null
+                    )
+                }
+                viewModelScope.launch {
+                    contactDataSource.insertContact(contact)
+                    delay(300L) // Animation delay
+                    newContact = null
+                }
+            } else {
+                _uiState.update {
+                    it.copy(
+                        firstNameError = result.firstNameError,
+                        lastNameError = result.lastNameError,
+                        emailError = result.emailError,
+                        phoneNumberError = result.phoneNumberError
+                    )
+                }
+            }
+
+        }
+    }
+
+    private fun selectContact(contact: Contact) {
+        _uiState.update {
+            it.copy(
+                selectedContact = contact,
+                isSelectedContactSheetOpen = true
+            )
+        }
+    }
+
+    private fun onPhotoPicked(value: ByteArray) {
+        newContact = newContact?.copy(
+            photoBytes = value
+        )
+    }
+
+    private fun onFirstNameChanged(value: String) {
+        newContact = newContact?.copy(
+            firstName = value
+        )
+    }
+
+    private fun onLastNameChanged(value: String) {
+        newContact = newContact?.copy(
+            lastName = value
+        )
+    }
+
+    private fun onPhoneNumberChanged(value: String) {
+        newContact = newContact?.copy(
+            phoneNumber = value
+        )
+    }
+
+    private fun onEmailChanged(value: String) {
+        newContact = newContact?.copy(
+            email = value
+        )
+    }
+
+    private fun onAddNewContactClick() {
+        _uiState.update {
+            it.copy(
+                isAddContactSheetOpen = true
+            )
+        }
+        newContact = Contact(
+            id = null,
+            firstName = "",
+            lastName = "",
+            email = "",
+            phoneNumber = "",
+            photoBytes = null
+        )
+    }
+
+    private fun editContact(contact: Contact) {
+        _uiState.update {
+            it.copy(
+                selectedContact = null,
+                isAddContactSheetOpen = true,
+                isSelectedContactSheetOpen = false
+            )
+        }
+        newContact = contact
+    }
+
+    private fun dismissContact() {
+        viewModelScope.launch {
+            _uiState.update {
+                it.copy(
+                    isSelectedContactSheetOpen = false,
+                    isAddContactSheetOpen = false,
+                    firstNameError = null,
+                    lastNameError = null,
+                    emailError = null,
+                    phoneNumberError = null
+                )
+            }
+            delay(300L) //Animation delay
+            newContact = null
+            _uiState.update {
+                it.copy(
+                    selectedContact = null
+                )
+            }
+        }
+    }
+
+    private fun deleteContact() {
+        viewModelScope.launch {
+            _uiState.value.selectedContact?.id?.let { id ->
+                _uiState.update {
+                    it.copy(isAddContactSheetOpen = false)
+                }
+                contactDataSource.deleteContact(id)
+                delay(300L) // Animation delay
+                _uiState.update {
+                    it.copy(selectedContact = null)
+                }
+            }
+        }
+    }
 
 }
