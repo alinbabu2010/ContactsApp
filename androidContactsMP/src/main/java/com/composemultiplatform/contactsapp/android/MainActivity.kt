54fbebd509b90f97aa348d4c8720834bbc65fd3d
Alin Babu <alinbabu2010@outlook.com>
2023-08-22 15:11:19 +0530

    Add app composable invocation for Android main activity

--- androidContactsMP/src/main/java/com/composemultiplatform/contactsapp/android/MainActivity.kt
+++ androidContactsMP/src/main/java/com/composemultiplatform/contactsapp/android/MainActivity.kt
@@ -3,12 +3,14 @@ package com.composemultiplatform.contactsapp.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import com.composemultiplatform.contactsapp.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            App(darkTheme = isSystemInDarkTheme(), dynamicColor = true)
         }
     }
 }
