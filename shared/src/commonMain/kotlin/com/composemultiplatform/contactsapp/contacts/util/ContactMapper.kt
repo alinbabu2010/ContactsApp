6dfc9c856d358d7698c53ed04c41c71a00d4c895
Alin Babu <alinbabu2010@outlook.com>
2023-09-01 18:11:08 +0530

    Make function as suspend and add imageStorage field for photoBytes

--- shared/src/commonMain/kotlin/com/composemultiplatform/contactsapp/contacts/util/ContactMapper.kt
+++ shared/src/commonMain/kotlin/com/composemultiplatform/contactsapp/contacts/util/ContactMapper.kt
@@ -1,15 +1,16 @@
 package com.composemultiplatform.contactsapp.contacts.util
 
 import com.composemultiplatform.contactsapp.contacts.domain.Contact
+import com.composemultiplatform.contactsapp.core.data.ImageStorage
 import database.ContactEntity
 
-fun ContactEntity.toContact(): Contact {
+suspend fun ContactEntity.toContact(imageStorage: ImageStorage): Contact {
     return Contact(
         id = id,
         firstName = firstName,
         lastName = lastName,
         email = email,
         phoneNumber = phoneNumber,
-        photoBytes = null
+        photoBytes = imagePath?.let { imageStorage.getImage(it) }
     )
 }
